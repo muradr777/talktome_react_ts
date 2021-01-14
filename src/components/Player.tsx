04face35432b10ba9d79f9f1233890b92ddcb747
Murad Rustamov <mrustamov@studenten.hs-bremerhaven.de>
2021-01-14 20:43:57 +0100

    Stable - Background music, youtube iframe ready

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,7 +1,7 @@
 import Youtube from "react-youtube";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export default function Player() {
+export default function Player({ playerState }: any) {
   const [opts] = useState<Object>({
     height: "390",
     width: "640",
@@ -10,10 +10,23 @@ export default function Player() {
       loop: 1,
     },
   });
+  const [player, setPlayer] = useState<any>(null);
 
   const onReady = (e: any) => {
-    e.target.playVideo();
+    setPlayer(e.target);
   };
+
+  useEffect(() => {
+    console.log(player, playerState);
+    if (player !== null) {
+      if (playerState) {
+        player.playVideo();
+        player.unMute();
+      } else {
+        player.mute();
+      }
+    }
+  }, [playerState]);
 
   return <Youtube videoId="L786lbUnZIY" opts={opts} onReady={onReady} />;
 }
